90d782c02f875ecbd73926908d328c72d3f3ca72
drnasmith <neil.smith@diamond.ac.uk>
2018-05-22 16:21:18 +0100

    Switched to use newer Apereo CAS Server

--- playbooks/roles/cas/files/scripts/generate_key.sh
+++ playbooks/roles/cas/files/scripts/generate_key.sh
@@ -1,3 +1,3 @@
-keytool -genkey -alias tomcat -keyalg RSA -validity 365 -dname "cn=Vagrant Vagrant, ou=Diamond, o=Diamond, c=UK" -storepass integration -keypass integration
-keytool -importkeystore -srckeystore /home/vagrant/.keystore -destkeystore /home/vagrant/.keystore -deststoretype pkcs12 -srcstorepass integration
-keytool -export -alias tomcat -file server.crt -storepass integration
+keytool -genkey -alias tomcat -keyalg RSA -validity 365 -dname "cn=Vagrant Vagrant, ou=Diamond, o=Diamond, c=UK" -storepass changeit -keypass changeit
+keytool -importkeystore -srckeystore /home/vagrant/.keystore -destkeystore /home/vagrant/.keystore -deststoretype pkcs12 -srcstorepass changeit
+keytool -export -alias tomcat -file server.crt -storepass changeit
